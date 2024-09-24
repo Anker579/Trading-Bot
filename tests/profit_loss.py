5948debe5c87d28fc43c5d2b3565f65654eccd64
Anker579 <angushoward03@gmail.com>
2024-09-24 22:49:21 +0100

    added images and explanation

--- tests/profit_loss.py
+++ tests/profit_loss.py
@@ -5,7 +5,7 @@ def calc_p_l(df):
     buy_price = 0
     index = 0
     prev_sig = 1
-    unique_sigs = 0
+    unique_sigs = 1
     profit_stream = {
         "time": [],
         "profit": []
@@ -17,17 +17,23 @@ def calc_p_l(df):
             if sig == 2:
                 buy_price = df["Close"][index]
                 prev_sig = 2
-                index += 1
+                #print(buy_price, index, "s")
             elif sig == 1:
                 sell_price = df["Close"][index]
+                #print(sell_price, index, "b")
                 prev_sig = 1
-                index += 1
             if unique_sigs % 2 == 0 and unique_sigs != 0:
-                profit += 1000*(sell_price-buy_price)
-                profit_stream["time"].append(df["time"][index])
+                #print(buy_price, sell_price)
+                #print(1000*(sell_price-buy_price)) 
+                #print(index)
+                #print(df["Datetime"][index])
+                
+                profit = 1000*(sell_price-buy_price)
+                profit_stream["time"].append(df["Datetime"][index])
                 profit_stream["profit"].append(profit)
             unique_sigs += 1
-            print(f"{sig}___{profit}")
+            #print(f"{sig}___{profit}")
+        index += 1
     
     prof_stream = pd.DataFrame.from_dict(profit_stream)
 
