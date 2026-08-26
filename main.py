042a8c24a5faf120d2c88bfa31affeda24dd2ea4
Anker579 <angushoward03@gmail.com>
2026-08-26 21:22:21 +0100

    corrected indexing in main.py part 2

--- main.py
+++ main.py
@@ -74,7 +74,15 @@ def make_trade():
 
     # Debug Code
     print("\n--- SIGNAL DEBUG ---")
-    print(completed_df[f"Close", f"sma_{config.SMA_WINDOWS[0]}", f"sma_{config.SMA_WINDOWS[1]}"].tail(3))
+    print(
+        completed_df[
+            [
+                "Close",
+                f"sma_{config.SMA_WINDOWS[0]}",
+                f"sma_{config.SMA_WINDOWS[1]}"
+            ]
+        ].tail(3)
+        )
     print(f"Signal: {signal}")
     print(f"Execution signal: {execution_signal}")
     print(f"Candle ID: {candle_id}")
